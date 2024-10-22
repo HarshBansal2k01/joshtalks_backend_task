6edb6d8b16610ac13336aa77befb5be60b63b5eb
HARSH BANSAL <ihbbro14@gmail.com>
2024-10-22 11:43:18 +0530

    added urls to home route also

--- apidevelopment/views.py
+++ apidevelopment/views.py
@@ -3,8 +3,47 @@ from django.http import HttpResponse, JsonResponse
 def home_page(request):
     print ("Home page")
     # return HttpResponse("Hello, world. You're at the home page.")
-    friends=[
-        'harsh','bansal'
+    apis= [
+        {
+        "method": "GET",
+        "url": "api/v1/users/",
+        "description": "List all users."
+        },
+        {
+        "method": "POST",
+        "url": "api/v1/users/",
+        "description": "Create a new user."
+        },
+        {
+        "method": "GET",
+        "url": "api/v1/tasks/",
+        "description": "List all tasks."
+        },
+        {
+        "method": "POST",
+        "url": "api/v1/tasks/",
+        "description": "Create a new task."
+        },
+        {
+        "method": "POST",
+        "url": "/usertasks/",
+        "description": "Assign a task to a user (or multiple users)."
+        },
+        {
+        "method": "GET",
+        "url": "/tasks-for-user/{user_id}/",
+        "description": "Retrieve tasks for a specific user."
+        },
+        {
+        "method": "GET",
+        "url": "/users-for-task/{task_id}/",
+        "description": "Retrieve users assigned to a specific task."
+        },
+        {
+        "method": "POST",
+        "url": "/status-update/",
+        "description": "Update the status of a task assigned to a user."
+        }
     ]
-    return JsonResponse(friends,safe=False)
+    return JsonResponse(apis,safe=False)
     
